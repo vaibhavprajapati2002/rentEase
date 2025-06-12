a8d6ffef89fc71fb576459936dea6e2c0c8cc9f6
Vivek20510 <vivekbadodiya68@gmail.com>
2025-06-12 14:41:32 +0530

    update
    Fix navbar

--- front_end/RentEase/src/MyComponent/CustomNavbar.jsx
+++ front_end/RentEase/src/MyComponent/CustomNavbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Logo from '../assets/images/logo.png';
 
@@ -15,22 +15,12 @@ const CustomNavbar = () => {
     navigate('/login');
   };
 
-  const avatarStyle = {
-    width: '35px',
-    height: '35px',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    marginRight: '10px',
-  };
-
-  const dummyAvatar = "https://cdn-icons-png.flaticon.com/512/147/147144.png";
-
   return (
     <Navbar bg="light" sticky="top" expand="lg" className="shadow-sm" style={{ height: '80px' }}>
       <Container>
         <Navbar.Brand as={Link} to={token ? `/${role}/dashboard` : '/'}>
           <img
-            src={Logo} 
+            src={Logo}
             alt="RentEase"
             style={{ height: '60px', width: '140px', objectFit: 'contain' }}
           />
@@ -51,28 +41,9 @@ const CustomNavbar = () => {
                 <Nav.Link as={Link} to="/contact">
                   <i className="bi bi-envelope"></i> Contact
                 </Nav.Link>
-
-                {/* Profile Dropdown */}
-                <NavDropdown
-                  title={
-                    <span>
-                      <Image src={dummyAvatar} alt="Avatar" style={avatarStyle} />
-                    </span>
-                  }
-                  id="profile-dropdown"
-                  align="end"
-                >
-                  <NavDropdown.Item as={Link} to={`/${role}/profile`}>
-                    <i className="bi bi-person-lines-fill"></i> View Profile
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to={`/${role}/settings`}>
-                    <i className="bi bi-gear"></i> Settings
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item onClick={handleLogout}>
-                    <i className="bi bi-box-arrow-right"></i> Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link onClick={handleLogout}>
+                  <i className="bi bi-box-arrow-right"></i> Logout
+                </Nav.Link>
               </>
             ) : (
               <>
